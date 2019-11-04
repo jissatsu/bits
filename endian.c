c62063776e246d6aa20f429eb44ec2a6051d91fb
jissatsu <ichi256@abv.bg>
2019-11-04 19:11:20 +0200

    for endianness sake...

---
+++ endian.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+// convert 32bit integer to big endian
+uint32_t host32BE( uint32_t num )
+{
+    uint32_t rtv;
+    
+    rtv = 0x00000000;
+    rtv = (((((((rtv >> 24) | (num & 0xff)) << 8) | ((num >> 8) & 0xff)) << 8) | ((num >> 16) & 0xff)) << 8) | ((num >> 24) & 0xff);
+    return rtv;
+}
+
+int main( int argc, char **argv )
+{
+    printf( "%u\n", host32BE( 3232235621 ) );
+    return 0;
+}
